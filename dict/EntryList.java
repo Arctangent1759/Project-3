20b2491f62739f60e7fea1a2e8502502397c4fe9
void- <mr.alex.guy@gmail.com>
2013-04-21 00:02:28 -0700

    Documentation + find() implementation

--- dict/EntryList.java
+++ dict/EntryList.java
@@ -1,5 +1,5 @@
 package dict;
-import DList.DList;
+import DList.*;
 
 /**
  *  Wrapper class for DList.DList for Entry objects.
@@ -7,6 +7,21 @@ import DList.DList;
  *  use of arrays containing generic objects.
  **/
 class EntryList extends DList<Entry> {
+  /**
+   *  find() tries to find a key within this entry list and 
+   *  return its corresponding value. If the given key is not
+   *  in this entry list, return null.
+   *
+   *  Running time: O(n), where n is the length of this entry list.
+   *  @param key the key to look for.
+   *  @return the value that corresponds with the given key.
+   **/
   Object find(Object key) {
+    for(Entry e:this) {
+      if (e.key.equals(key)) {
+        return e.value;
+      }
+    }
+    return null;
   }
 }
