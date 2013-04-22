9544777f0f32436ebf41928b1a9b867c38398f29
Alexander Chu <arctangent1759@ubuntu.(none)>
2013-04-21 23:44:57 -0700

    microwave

--- GraphStruct/GraphStruct.java
+++ GraphStruct/GraphStruct.java
@@ -1,4 +1,9 @@
-import DList.DList;
-import DList.DListNode;
+package GraphStruct;
+import GraphStruct.EdgeList.EdgeList;
+import GraphStruct.VertexList.VertexList;
+import dict.Hashtable;
 public class GraphStruct{
+  Hashtable edgeTable;
+  Hashtable vertexTable;
+  VertexList vertices;
 }
