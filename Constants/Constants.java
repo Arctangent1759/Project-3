469b36dae33430e8ce581c3b7f9b37ce513ec911
void- <mr.alex.guy@gmail.com>
2013-04-22 00:29:22 -0700

    Better documentation

--- Constants/Constants.java
+++ Constants/Constants.java
@@ -1,4 +1,7 @@
 package Constants;
+/**
+ *  Constants class is a container for all constants.
+ **/
 public final class Constants{
 
   /**
@@ -13,10 +16,12 @@ public final class Constants{
   public static final float MIN_LOAD = 0.5f;
 
 	/**
-	 *
-	 * Prints object o to standard error.
-	 * @param o The object to be printed
-	 *
+	 *  print() prints object o to standard error. 
+   *  If and only if DEBUG is set to true. This
+   *  ensures that no extra debug statements will 
+   *  mess with output to standard out.
+   *
+	 *  @param o object to be printed.
 	 */
 	public static final void print(Object o){
 		if (DEBUG){
