46031c4b9ecf0949a13435c06b01126bc5b2e940
void- <mr.alex.guy@gmail.com>
2013-04-21 22:12:01 -0700

    added remove() method for Hashtable.remove()

--- dict/EntryList.java
+++ dict/EntryList.java
@@ -7,6 +7,7 @@ import DList.*;
  *  use of arrays containing generic objects.
  **/
 class EntryList extends DList<Entry> {
+
   /**
    *  find() tries to find a key within this entry list and 
    *  return its corresponding value. If the given key is not
@@ -23,5 +24,21 @@ class EntryList extends DList<Entry> {
       }
     }
     return null;
+  }
+
+  /**
+   *  remove() takes a key, finds it, and removes it from this entry list.
+   *  If the given key is not in this entry list, nothing happens.
+   *
+   *  @param key the key of the entry to remove.
+   **/
+  void remove(Object key) {
+    java.util.Iterator<Entry> i = this.iterator();
+    //use this sytle of iteration to have access to DListIterator remove()
+    while(i.hasNext()) {
+      if((i.next()).getKey().equals(key)) {
+        i.remove();
+      }
+    }
   }
 }
