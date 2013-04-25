8e439481d62bfff02577a7b7ce136b53dcb05cd9
Nitin Sadras <nitin.sadras@gmail.com>
2013-04-24 23:11:00 -0700

    getEdges now works

--- Kruskal.java
+++ Kruskal.java
@@ -4,6 +4,7 @@ import graph.*;
 import set.*;
 import dict.*;
 import DList.*;
+import Constants.*;
 
 /**
  * The Kruskal class contains the method minSpanTree(), which implements
@@ -21,11 +22,12 @@ public class Kruskal {
     for (Object o:g.getVertices()){
       out.addVertex(o);
     }
-    DList<EdgeWrapper> edges = getEdges(g.getVertices()[0],g,new Hashtable(g.vertexCount()),new DList<EdgeWrapper>());
+    //DList<EdgeWrapper> edges = getEdges(g.getVertices()[0],g,new Hashtable(g.vertexCount()),new DList<EdgeWrapper>());
     //TODO: implement implementation
     return null;
   }
-
+ 
+  /*
   private static DList<EdgeWrapper> getEdges(Object curr, WUGraph g, Hashtable visited, DList<EdgeWrapper> out){
     //TODO: This is wrong.
     visited.insert(curr,"Alan Turing Lives.");
@@ -36,6 +38,44 @@ public class Kruskal {
       }
     }
     return out;
+  }
+  */
+
+  private static EdgeWrapper[] getEdges(WUGraph g){
+    Object[] vertices = g.getVertices();  // list of vertices
+    Hashtable visited = new Hashtable();  // list of visited edges
+    EdgeWrapper[] edges = new EdgeWrapper[g.edgeCount()]; // list of edges to be returned
+    int index = 0;
+    for(Object v : vertices){
+      Neighbors neighbors = g.getNeighbors(v);
+      for(int i = 0; i < neighbors.neighborList.length; i++){
+        EdgeWrapper curr = new EdgeWrapper(v,neighbors.neighborList[i], neighbors.weightList[i]); 
+        // if the edge hasn't been visited, add it to the list
+        if(visited.find(curr) == null){ 
+          visited.insert(curr,neighbors.weightList[i]); 
+          edges[index] = curr;
+          index += 1;
+        }
+      }
+    }
+    return edges;
+  }
+
+  public static void main(String[] args){
+    WUGraph w = new WUGraph();
+    w.addVertex(1);
+    w.addVertex(2);
+    w.addVertex(3);
+    w.addVertex(4);
+    w.addEdge(1,2,1);
+    w.addEdge(1,3,1);
+    w.addEdge(1,4,1);
+    w.addEdge(4,3,1);
+
+    EdgeWrapper[] edges = Kruskal.getEdges(w);
+    for(EdgeWrapper edge: edges){
+      Constants.print("V1: " + edge.v1 + " V2: " + edge.v2 + " weight: " + edge.weight);
+    }
   }
   
 }
@@ -49,4 +89,29 @@ class EdgeWrapper{
     this.v2=v2;
     this.weight=weight;
   }
+  public int hashCode() {
+    if (v1.equals(v2)) {
+      return v1.hashCode() + 1;
+    } else {
+      return v1.hashCode() + v2.hashCode();
+    }   
+  }
+
+  /** 
+   * equals() returns true if this VertexPair represents the same unordered
+   * pair of objects as the parameter "o".  The order of the pair does not
+   * affect the equality test, so (u, v) is found to be equal to (v, u).
+   */
+  public boolean equals(Object o) {
+    if (o instanceof EdgeWrapper) {
+      return ((v1.equals(((EdgeWrapper) o).v1)) &&
+              (v2.equals(((EdgeWrapper) o).v2))) ||
+             ((v1.equals(((EdgeWrapper) o).v2)) &&
+              (v2.equals(((EdgeWrapper) o).v1)));
+    } else {
+      return false;
+    }   
+  }
 }
+
+
