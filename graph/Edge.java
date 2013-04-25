5189a237b130af78dc64ce72870fd3b33c9db48f
void- <mr.alex.guy@gmail.com>
2013-04-25 00:35:52 -0700

    added compareTo() for sorting algorithms.

--- graph/Edge.java
+++ graph/Edge.java
@@ -1,7 +1,7 @@
 package graph;
 import DList.*;
 
-public class Edge{
+public class Edge implements Comparable {
     public Vertex v1;
     public Vertex v2;
     int weight;
@@ -33,5 +33,30 @@ public class Edge{
 
     public VertexPair vertices(){
       return new VertexPair(v1,v2);
+    }
+
+    /**
+     *  compareTo() compares this edge with another object o and returns
+     *  a numerical representation of their weight differences. 
+     *  This follows the properties outlined in the Java API such that:
+     *   -Object o must be an edge; if not, return -255. This is done
+     *      rather than throwing a ClassCastException.
+     *   -If object o has a weight greater than this edge: -1
+     *   -If object o has a weight less    than this edge:  1
+     *   -If object o has a weight equal   to   this edge:  0
+     *  
+     *  @param o the Object to be compared to.
+     *  @return the numerical representation of the ordering of this and o.
+     **/
+    public int compareTo(Object o) {
+        if(o !instanceof Edge) {
+            return -255;
+        } else if (this.weight == ((Edge)o).weight) {
+            return 0;
+        } else if(this.weight > ((Edge)o).weight) {
+            return 1;
+        } else {  //o must be greater than this
+            return -1;
+        }
     }
 }
