46b0b2a054baf2a9907cb0b581b49857f7675ae2
Nitin Sadras <nitin.sadras@gmail.com>
2013-05-01 01:52:02 -0700

    documentation

--- graph/Vertex.java
+++ graph/Vertex.java
@@ -1,9 +1,23 @@
 package graph;
 import DList.*;
 
-public class Vertex{
+/**
+ *
+ * Internal representation of vertices
+ *
+ **/
+
+class Vertex{
   EdgeList edges;
   Object item;
+
+  /**
+   *
+   * Constructors creates a new vertex with an object and an empty EdgeList
+   *
+   * @param item the object at this vertex
+   *
+   **/
   public Vertex(Object item){
     this.item = item;
     edges = new EdgeList();
