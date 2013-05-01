b9212e76e2d919cab9b31140be53fea6826aea20
void- <mr.alex.guy@gmail.com>
2013-05-01 01:11:07 -0700

    More documentation; additional annotations; Documentation still unfinished

--- sort/SortEdge.java
+++ sort/SortEdge.java
@@ -1,11 +1,15 @@
 package sort;
 import Constants.Constants;
+import graph.Edge;
 
+/**
+ *  SortEdge class is functionally available as a means to
+ *  sort an array of comparable Edges.
+ **/
 public class SortEdge {
   public static void sort(Comparable a[]){
     quicksort(a);
   }
-
 
   /**
    *  quicksort()[1 param] is a driver for quicksort()[3 param].
@@ -19,13 +23,15 @@ public class SortEdge {
 
   /**
    *  quicksort() sorts an array of comparable items from least to
-   *  greatest using the quicksort algorithm.
+   *  greatest using the quicksort algorithm. The given array will
+   *  be mutated.
    *
    *  Running time: O(nlogn)
    *  @param a array of comparable items to be sorted.
    *  @param low the lower bound of the partition to be sorted.
    *  @param high the upper bound of the partition to be sorted.
    **/
+  @SuppressWarnings("unchecked")
   public static void quicksort(Comparable a[], int low, int high) {
     if (low < high) {
       int pivotIndex = randomPivot(low, high);
@@ -33,7 +39,6 @@ public class SortEdge {
       Comparable swap; //Use this for swapping a[i] and a[j]
       a[pivotIndex] = a[high]; //Swap pivot with last item
       a[high] = pivot;
-
 
       int i = low - 1;
       int j = high;
@@ -76,7 +81,7 @@ public class SortEdge {
     return out+"]";
   }
 
-  private static String arrStr(Comparable[] a,int l,int h){
+  private static String arrStr(Comparable[] a, int l, int h){
     String out = "[";
     for (int i = l; i<=h; i++){
       out+=" "+a[i].toString()+" ";
@@ -84,13 +89,14 @@ public class SortEdge {
     return out+"]";
   }
 
-
-
   /**
-   *  radixSort() sorts an array of comparable items from least to
-   *  greatest using the radix sorting algorithm.
+   *  radixSort() sorts an array of Edge items from least to
+   *  greatest based upon weight using the radix sorting algorithm.
+   *  The given array will be mutated.
+   *  
+   *  @param a Edge array containing the edges to be sorted.
    **/
-  public static void radixSort(Object a[]) {
+  public static void radixSort(Edge a[]) {
     //TODO:implement
   }
 }
